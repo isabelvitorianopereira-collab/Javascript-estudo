701023a7600fd9e2b1d2e7a04815ed1caf2057f7
Bel <isabelvitorianopereira@gmail.com>
2026-04-14 15:38:54 -0300

    dois exercicios

---
+++ estutura_sequencial/ant_prox.js
@@ -0,0 +1,6 @@
+let a = 50;
+
+let anterior = a - 1;
+let proximo = a + 1;
+
+console.log(`o antecessor de ${a} é ${anterior} e seu sucessor é ${proximo}`);
---
+++ estutura_sequencial/soma.js
@@ -0,0 +1,6 @@
+let a = 2;
+let b = 10;
+
+let soma = a+b;
+
+console.log(`A soma dos números é igual a ${soma}`);
